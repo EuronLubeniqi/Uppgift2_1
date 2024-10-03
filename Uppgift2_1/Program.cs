bcd8ce81214a2da24cb9791861243bc8b0115771
EuronLubeniqi <eulu07001@utb.vaxjo.se>
2024-10-03 13:47:55 +0200

    Add project files.

---
+++ Uppgift2_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Uppgift2_1
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("vilken är din favoritbok");
+            string favoritbok = Console.ReadLine();
+
+            Console.WriteLine(favoritbok);
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
